975f12b964381392ece9396052cc714e1874f340
s32989 <s32989@gmail.com>
2021-01-07 12:46:10 -0500

    api call commented

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 //const bodyParser = require('body-parser');
+//https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT
 
 const app = express();
 
 app.use('/', (req, res, next) => {
-    
+
     console.log("hello")
 })
 
-module.exports = app; 
+module.exports = app;
