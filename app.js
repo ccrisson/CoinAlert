5ef5aa9ee862bfa6e02c546f88817d3a6b676123
Christopher <crisson.christopher@gmail.com>
2021-01-07 14:15:33 -0500

    Price data is aquired

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 //const bodyParser = require('body-parser');
 //https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT
+const axios = require('axios');
 
 const app = express();
 
 app.use('/', (req, res, next) => {
-
-    console.log("hello")
+    axios.get('https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT')
+        .then(function (response) {
+            console.log(response.data.price);
+        })
+    console.log("did it work?")
 })
 
 module.exports = app;
