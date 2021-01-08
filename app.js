800f2bd16c3a2a214895a0371993e88fa86f89dc
s32989 <s32989@gmail.com>
2021-01-08 11:46:23 -0500

    cron job in place for email to sent

--- app.js
+++ app.js
@@ -7,45 +7,43 @@ const app = express();
 const cron = require('node-cron');
 const emailconfig = require('./emailconfig');
 
-cron.schedule('* * * * *', function () {
-console.log('Testing');
+cron.schedule('*/5 * * * *', function () {
+  axios.get('https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT')
+      .then(function (response) {
+          console.log(response.data.price);
+
+          if(response.data.price < 38200){  //change back to less than
+
+            const transporter = nodemailer.createTransport({
+              service: 'gmail',
+              auth: {
+                user: emailconfig.user,
+                pass: emailconfig.pw
+              }
+            });
+
+            const mailOptions = {
+              from: emailconfig.user,
+              to: emailconfig.number,
+              subject: 'RHED ALERT!!! TONYS HOME',
+              text: response.data.price
+            };
+
+            transporter.sendMail(mailOptions, function(error, info){
+              if (error) {
+                console.log(error);
+              } else {
+                console.log('Email sent: ' + info.response);
+              }
+            });
+
+          }
+
+      })
+
 });
 
 
-app.use('/', (req, res, next) => {
-    axios.get('https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT')
-        .then(function (response) {
-            console.log(response.data.price);
 
-            if(response.data.price < 37200){
-
-              const transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                  user: emailconfig.user,
-                  pass: emailconfig.pw
-                }
-              });
-
-              const mailOptions = {
-                from: emailconfig.user,
-                to: emailconfig.number,
-                subject: 'RHED ALERT!!! TONYS HOME',
-                text: response.data.price
-              };
-
-              transporter.sendMail(mailOptions, function(error, info){
-                if (error) {
-                  console.log(error);
-                } else {
-                  console.log('Email sent: ' + info.response);
-                }
-              });
-
-            }
-
-        })
-    console.log("did it work?")
-})
 
 module.exports = app;
